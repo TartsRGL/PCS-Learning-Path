749b0d9be701d0d58adf39f10df7dc2fa4bbd6fb
TartsRGL <tarts.rgl@gmail.com>
2026-05-23 15:32:46 +0200

    chore: remove migrated PCS-FAPP project

--- PCS-FAPP/ver3/dashboard/script.js
+++
@@ -1,8 +0,0 @@
-function toggleDetails(projectId) {
-    const projectDetails = document.getElementById(projectId);
-    if (projectDetails.style.display === "none" || projectDetails.style.display === "") {
-        projectDetails.style.display = "block";
-    } else {
-        projectDetails.style.display = "none";
-    }
-}
